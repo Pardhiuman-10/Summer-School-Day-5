c8e04e100fdee9f75ef65be9839b5a22cca9fe88
Pardhiuman-10 <pardhiuman123@gmail.com>
2025-06-30 23:20:21 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,19 @@
+// string
+let str="ON TOP";
+console.log("Type is =",typeof str);
+
+
+
+const pi=3.14;
+console.log("Type is =",typeof pi);
+
+
+
+var isStudentPresent=true;
+console.log("Type is =",typeof isStudentPresent);
+
+
+
+
+console.log(" Type is =",typeof l);
+
---
+++ task6.js
@@ -0,0 +1,8 @@
+
+
+function processNumber(num, callback) {
+  callback(num);
+}
+processNumber(5, function(n) {
+  console.log("Double is", n * 2);
+});
